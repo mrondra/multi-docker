cc404bb4b35a9eef6e3fb3ef773052407f77d935
Ondřej Zeman <ondrej.zeman@symbio.agency>
2019-03-14 13:17:52 +0100

    text change

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
                     <header className="App-header">
                         <img src={logo} className="App-logo" alt="logo" />
                         <p>
-                            Edit <code>src/App.js</code> and save to reload.
+                            Fib Calculator
                         </p>
                         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                             Learn React
